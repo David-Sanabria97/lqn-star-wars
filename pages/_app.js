2d76103b251a4ec044531864cbda354e558c8f60
David-Sanabria97 <daviidsanabria97@gmail.com>
2021-12-28 21:20:35 -0500

    primer commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../services/apollo-cliente";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />{" "}
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
