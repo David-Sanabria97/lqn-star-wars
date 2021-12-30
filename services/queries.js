ac53d4ecf7453f4e5e1bcd7db08421e5298b3794
DavidSGamboa <david.sanabria@pcsoluciones.com.co>
2021-12-29 22:53:21 -0500

    agregar queries de peliculas y planetas

--- services/queries.js
+++ services/queries.js
@@ -33,3 +33,30 @@ export const GET_PERSON = gql`
     }
   }
 `;
+
+export const GET_PERSON_FILMS = gql`
+  query getPersonFilms($id: ID) {
+    person(id: $id) {
+      filmConnection {
+        films {
+          id
+          title
+          director
+        }
+      }
+    }
+  }
+`;
+
+export const GET_PLANETS_FILM_ = gql`
+  query getPlanetsFilm($id: ID) {
+    film(id: $id) {
+      planetConnection {
+        planets {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
